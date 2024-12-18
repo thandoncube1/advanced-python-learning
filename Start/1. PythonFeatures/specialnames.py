05ece49aaf21731c109885beb69e6497f96d718a
thandoncube1 <ncubethando20@gmail.com>
2024-12-18 16:34:14 -0500

    special - names are going to be used.

--- Start/1. PythonFeatures/specialnames.py
+++ Start/1. PythonFeatures/specialnames.py
@@ -3,9 +3,10 @@
 
 
 # __name__ is the name of the module
-
+print("Module name: ", __name__)
 
 # __file__ contains the path to the file from which the module was loaded
-
+print("File name: ", __file__)
 
 # __package__ indicates the package that the module belongs to.
+print("Package name: ", __package__)
